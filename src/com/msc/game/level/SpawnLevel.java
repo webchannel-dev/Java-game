eb297712bff07617071397284514933e4e96da98
AJ <42003122+code7885@users.noreply.github.com>
2013-10-29 16:40:48 +0400

    53 - Pixels to Tiles

--- src/com/msc/game/level/SpawnLevel.java
+++ src/com/msc/game/level/SpawnLevel.java
@@ -20,13 +20,27 @@ public class SpawnLevel extends Level {
             BufferedImage img = ImageIO.read(getClass().getClassLoader().getResource(path));
             int w = img.getWidth();
             int h = img.getHeight();
+            tiles = new Tile[w * h];
             img.getRGB(0, 0, w, h, levelPixels, 0, w);
         } catch (IOException ex) {
             Logger.getLogger(SpawnLevel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
     }
 
+    //Grass=0xFFOO
+    //Flower=0xFFFF00
+    //Rock=0x7F700
     protected void generateLevel() {
-        
+        for (int i = 0; i < levelPixels.length; i++) {
+            if (levelPixels[i] == 0xff00) {
+                tiles[i] = Tile.grass;
+            }
+            if (levelPixels[i] == 0xFFFF00) {
+                tiles[i] = Tile.flower;
+            }
+            if (levelPixels[i] == 0x7F700) {
+                tiles[i] = Tile.rock;
+            }
+        }
     }
 }
